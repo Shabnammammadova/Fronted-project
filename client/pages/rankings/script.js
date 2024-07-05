75ffc61af261fa79b1672aee14c63c79090bda3b
shabnam <shabnamjm@code.edu.az>
2024-07-05 15:30:17 +0400

    ranking page design and function added

--- client/pages/rankings/script.js
+++ client/pages/rankings/script.js
@@ -3,6 +3,7 @@ const signupBtn = document.querySelector(".signup-btn");
 const loginBtn = document.querySelector(".login-btn");
 const marketplace = document.querySelector(".marketplace");
 
+
 const burgerNavbarlogo = document.getElementById("burgerIcon");
 const burgerMenu = document.querySelector(".navbar.burger-menu");
 
@@ -33,5 +34,45 @@ burgerNavbarlogo.addEventListener('click', ()=>{
     if(window.innerWidth>1280){
       burgerMenu.classList.remove('open')
     }
-  })
+})
   
+
+
+
+const aboutCreators = document.querySelector(".about-creators");
+
+
+async function getCreators() {
+    const response = await fetch(`${BASE_URL}/creators`);
+    const data = await response.json();
+    return data;
+  }
+  
+  getCreators().then((data) => fillCard(data));
+  
+  async function fillCard(creators) {
+    creators.forEach((creator) => {
+      const aboutCards = document.querySelector(".about-creators");
+      const cardElement = document.createElement("div");
+      cardElement.classList.add("creators");
+      cardElement.innerHTML += `
+             <div class="creators">
+            <div class="creator-info">
+<div class="creator">
+    <p>${creator.id}</p>
+    <img src="/${creator.profileImgPath}" alt="">
+    <span>${creator.name}</span>
+</div>
+<div class="creator-nft">
+    <p class="p-change">+${creator.totalSale.value}%</p>
+    <p class="p-nft">${creator.nftSold}</p>
+    <p class="p-volume">${creator.volume} ETH</p>
+</div>
+            </div>
+        </div>
+  
+      `;
+      aboutCards.append(cardElement);
+  
+    });
+}
